8881ed68dec8a013fbcf2ea407d54d8ce0de6b8d
luschee <lukasscheerer89@gmail.com>
2025-07-10 16:52:29 +0200

    Change app name

--- app/src/androidTest/java/de/scovillo/playondlna/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/scovillo/playondlna/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.example.upnpdlna", appContext.packageName)
+        Assert.assertEquals("de.scovillo.playondlna", appContext.packageName)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,5 +19,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "UpnpDlna"
+rootProject.name = "PlayOnDlna"
 include(":app")
