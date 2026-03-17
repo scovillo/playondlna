b479586df0220ce88bf41bdbe2ccfbc101a3c55b
luschee <lukasscheerer89@gmail.com>
2026-03-17 16:37:29 +0100

    Clean up Theme

--- app/src/main/java/io/github/scovillo/playondlna/theme/PlayOnDlnaTheme.kt
+++ app/src/main/java/io/github/scovillo/playondlna/theme/PlayOnDlnaTheme.kt
@@ -1,27 +1,37 @@
 package io.github.scovillo.playondlna.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
+import android.view.WindowInsets
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Color.White,
+private val darkColorScheme = darkColorScheme(
+    primary = Color.Red,
     onPrimary = Color.Black,
     background = Color.Black,
     onBackground = Color.White,
     surface = Color.DarkGray,
-    onSurface = Color.White
+    onSurface = Color.White,
 )
 
 @Composable
 fun PlayOnDlnaTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+        val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+        view.setBackgroundColor(darkColorScheme.background.toArgb())
+        view.setPadding(0, statusBarInsets.top, 0, 0)
+        insets
+    }
     MaterialTheme(
-        colorScheme = DarkColorScheme,
+        colorScheme = darkColorScheme,
         content = content
     )
 }
