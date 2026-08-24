0de6251999e6718d73003bdb3ff872b0955fa439
luschee <lukasscheerer89@gmail.com>
2026-08-24 22:13:06 +0200

    Support Android 8 (minSdk 26) (Ref #45)

--- app/src/main/java/io/github/scovillo/playondlna/ui/PlayOnDlnaTheme.kt
+++ app/src/main/java/io/github/scovillo/playondlna/ui/PlayOnDlnaTheme.kt
@@ -1,13 +1,14 @@
 package io.github.scovillo.playondlna.ui
 
 import android.app.Activity
-import android.view.WindowInsets
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 private val darkColorScheme =
     darkColorScheme(
@@ -23,8 +24,8 @@ private val darkColorScheme =
 fun playOnDlnaTheme(content: @Composable () -> Unit) {
     val view = LocalView.current
     val window = (view.context as Activity).window
-    window.decorView.setOnApplyWindowInsetsListener { view, insets ->
-        val statusBarInsets = insets.getInsets(WindowInsets.Type.statusBars())
+    ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+        val statusBarInsets = insets.getInsets(WindowInsetsCompat.Type.statusBars())
         view.setBackgroundColor(darkColorScheme.background.toArgb())
         view.setPadding(0, statusBarInsets.top, 0, 0)
         insets
